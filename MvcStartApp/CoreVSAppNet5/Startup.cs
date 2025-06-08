83c5d5c02d5ccb3819f1a91ddda080a064ce3969
aveA <aveA@AVE>
2025-06-08 12:33:53 +0300

    +

--- MvcStartApp/CoreVSAppNet5/Startup.cs
+++ MvcStartApp/CoreVSAppNet5/Startup.cs
@@ -16,6 +16,18 @@ namespace CoreVSAppNet5
     //ConfigureServices(опционально)
     public class Startup
     {
+
+        //Метод-конструктор
+        //При желании мы можем вызывать в классе Startup метод-конструктор.
+        //К примеру, если мы добавим следующий код. Мы сможем пробрасывать и сохранять переменные среды.
+        IWebHostEnvironment _env;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+
         // Метод вызывается средой ASP.NET.
         // Используйте его для подключения сервисов приложения
         // Документация:  https://go.microsoft.com/fwlink/?LinkID=398940
@@ -54,27 +66,85 @@ namespace CoreVSAppNet5
             //Здесь наше приложение получает возможность назначать входящие запросы определённым маршрутам
             app.UseRouting();
 
+
+            //Метод Use - тоже позволяет добавить компоненты Middleware в конвейер, но приводит к вызову следующего компонента
+            //Перегрузка метода может передавать обработку следующему компоненту в конвейере.
+            //Для этого можно использовать перегрузку метода Use, принимающую в качестве параметров контекст запроса HttpContext,
+            //и делегат, ссылающийся на следующий компонент в конвейере
+            //Добавляем компонент для логирования запросов с использованием метода Use.
+            //app.Use(async (context, next) =>
+            //{
+            //    // Для логирования данных о запросе используем свойства объекта HttpContext
+            //    Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+            //    await next.Invoke();
+            //});
+
+            //Метод Map - (а также методы-расширения, начинающиеся с этого слова) сопоставляют путь запроса с делегатом — обработчиком.
+            //app.Map("/about", About);
+
+            // Метод Run — это самый простой способ добавления компонента Middleware
+            // Добавим в конвейер запросов обработчик самым простым способом
+            // Компонент, добавленный таким образом, препятствует передаче запроса далее по конвейеру, то есть обработка на нём прекратится.
+            // (UseEndpoints() не увидим, если  перенесем за UseEndpoints(), то увидим)
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync($"Welcome to the {env.ApplicationName}!");
+            //});
+
+
             // 3. Добавляем компонент с настройкой маршрутов (EndpointMiddleware, который перехватывает запрос, проверяет его на соответствие определённому паттерну)
             //Здесь определит маршруты, обрабатываемые приложением
             //Тут определено, что все входящие запросы по маршруту "/", то есть непосредственно на главную страницу нашего приложения,
             //получат в ответ строку "Hello World!"
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/config", async context =>
+            //    {
+            //        await context.Response.WriteAsync($"Hello World! {env.EnvironmentName}");
+            //    });
+            //});
+
+
+            //Добавляем компонент с настройкой маршрутов для главной страницы
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync($"Hello World! {env.EnvironmentName}");
+                    await context.Response.WriteAsync($"Welcome to the {env.ApplicationName}!");
                 });
+            });
+
+            // Все прочие страницы имеют отдельные обработчики
+            app.Map("/about", About);
+            app.Map("/config", Config);
+
+            // Обработчик для ошибки "страница не найдена"
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Page not found");
+            });
+        }
+
+        /// <summary>
+        ///  Обработчик для страницы About
+        /// </summary>
+        private void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"{_env.ApplicationName} - ASP.Net Core tutorial project");
+            });
+        }
+
+        /// <summary>
+        ///  Обработчик для главной страницы
+        /// </summary>
+        private void Config(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"App name: {_env.ApplicationName}. App running configuration: {env.EnvironmentName}");
             });
         }
     }
-
-    //Метод-конструктор
-    //При желании мы можем вызывать в классе Startup метод-конструктор.
-    //К примеру, если мы добавим следующий код. Мы сможем пробрасывать и сохранять переменные среды.
-
-    //IWebHostEnvironment _env;
-    //public Startup(IWebHostEnvironment env)
-    //{
-    //    _env = env;
-    //}
 }
